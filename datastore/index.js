ec60348e49753f6dbae27264fe9daa0983c03c5e
Drew Henderson <drewsworld16@gmail.com>
2022-09-27 16:40:10 -0500

    Complete Bare Minimum / Update

--- datastore/index.js
+++ datastore/index.js
@@ -85,25 +85,23 @@ exports.update = (id, text, callback) => {
       callback('File Doesn\'t exist');
     }
   })
-
-  // var item = items[id];
-  // if (!item) {
-  //   callback(new Error(`No item with id: ${id}`));
-  // } else {
-  //   items[id] = text;
-  //   callback(null, { id, text });
 };
 
 exports.delete = (id, callback) => {
   //fs.rm(`./dataDir/${id}.txt`, (err)=>{[same error as below]}) or fs.unlink
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  let destination = path.join(exports.dataDir, id + '.txt');
+  fs.rm(destination, (err) => {
+    if (err) { callback(new Error(`No item with id: ${id}`)) }
+    else { callback() }
+  });
+  // var item = items[id];
+  // delete items[id];
+  // if (!item) {
+  //   // report an error if item not found
+  //   callback(new Error(`No item with id: ${id}`));
+  // } else {
+  //   callback();
+  // }
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
